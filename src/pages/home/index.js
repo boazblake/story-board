bd0249b7cfd7ee37a670cc32980d7c3fedef4cdd
Boaz Blake <bblake@goempyrean.com>
2024-01-21 16:30:04 -0600

    build

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -95,7 +95,7 @@ export const Home = ({ attrs: { mdl } }) => {
         label: "Select a File",
         labelPlacement: "stacked",
         type: "file",
-        accept: ["audio/*,video/*"],
+        accept: ["audio/*,audio/m4a"],
         style: { width: '150px' },
       }), 'Add Audio')
       )),
